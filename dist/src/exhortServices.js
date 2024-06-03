4234f9080f3a954b18a6236f799131a1681f38bb
Ilona Shishov <Ilona.Shishov@gmail.com>
2024-06-03 15:19:24 +0300

    chore: include build files
    Signed-off-by: Ilona Shishov <Ilona.Shishov@gmail.com>

---
+++ dist/src/exhortServices.js
@@ -0,0 +1,58 @@
+import exhort from '@rhecosystemappeng/exhort-javascript-api';
+import { execSync } from 'child_process';
+import path from 'path';
+/**
+ * Executes RHDA image analysis using the provided images and options.
+ * @param images - The images to analyze.
+ * @param options - The options for running image analysis.
+ * @returns A Promise resolving to the analysis response in HTML format.
+ */
+function imageAnalysisService(images, options) {
+    return new Promise(async (resolve, reject) => {
+        const __dirname = path.resolve();
+        const jarPath = `${__dirname}/javaApiAdapter/exhort-java-api-adapter-1.0-SNAPSHOT-jar-with-dependencies.jar`;
+        const reportType = 'json';
+        let parameters = '';
+        let properties = '';
+        images.forEach(image => {
+            if (image.platform) {
+                parameters += ` ${image.image}^^${image.platform}`;
+            }
+            else {
+                parameters += ` ${image.image}`;
+            }
+        });
+        for (const setting in options) {
+            if (options[setting]) {
+                properties += ` -D${setting}=${options[setting]}`;
+            }
+        }
+        try {
+            const result = execSync(`java${properties} -jar ${jarPath} ${reportType}${parameters}`, {
+                maxBuffer: 1000 * 1000 * 10, // 10 MB
+            });
+            resolve(result.toString());
+        }
+        catch (error) {
+            reject(error);
+        }
+    });
+}
+/**
+ * Performs RHDA stack analysis based on the provided manifest path and options.
+ * @param pathToManifest The path to the manifest file for analysis.
+ * @param options Additional options for the analysis.
+ * @returns A promise resolving to the stack analysis report in HTML format.
+ */
+async function stackAnalysisService(pathToManifest, options) {
+    try {
+        // Get stack analysis in JSON format
+        const stackAnalysisReportJson = await exhort.stackAnalysis(pathToManifest, false, options);
+        return stackAnalysisReportJson;
+    }
+    catch (error) {
+        throw error;
+    }
+}
+export { stackAnalysisService, imageAnalysisService };
+//# sourceMappingURL=exhortServices.js.map
